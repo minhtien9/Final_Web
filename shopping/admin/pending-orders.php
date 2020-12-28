9255b9abfb9d67a44ea69673ef1339c2c1b7f413
BinhBinh1812 <tranthaibinh0@gmail.com>
2020-12-28 23:25:38 +0700

    up thêm mấy cái order

--- shopping/admin/pending-orders.php
+++ shopping/admin/pending-orders.php
@@ -18,7 +18,7 @@ $currentTime = date( 'd-m-Y h:i:s A', time () );
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Admin| Pending Orders</title>
+	<title>Admin| Đơn hàng chờ xử lí</title>
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="css/theme.css" rel="stylesheet">
@@ -49,14 +49,14 @@ popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status
 
 	<div class="module">
 							<div class="module-head">
-								<h3>Pending Orders</h3>
+								<h3>Đơn hành chờ xử lí</h3>
 							</div>
 							<div class="module-body table">
 	<?php if(isset($_GET['del']))
 {?>
 									<div class="alert alert-error">
 										<button type="button" class="close" data-dismiss="alert">×</button>
-									<strong>Oh snap!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
+									<strong>Lỗi!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
 									</div>
 <?php } ?>
 
@@ -67,14 +67,14 @@ popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status
 									<thead>
 										<tr>
 											<th>#</th>
-											<th> Name</th>
-											<th width="50">Email /Contact no</th>
-											<th>Shipping Address</th>
-											<th>Product </th>
+											<th> Tên</th>
+											<th width="50">Email /Liên hệ</th>
+											<th>Địa chỉ giao hàng</th>
+											<th>Sản phẩm </th>
 											<th>Qty </th>
-											<th>Amount </th>
-											<th>Order Date</th>
-											<th>Action</th>
+											<th>Giá </th>
+											<th>Ngày đặt hàng</th>
+											<th>Hình thức vận chuyển</th>
 											
 										
 										</tr>
