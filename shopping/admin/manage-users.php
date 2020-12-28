e730aef74cd9d48720065a3a85b19457b79dfb0c
BinhBinh1812 <tranthaibinh0@gmail.com>
2020-12-28 13:04:54 +0700

    up trước manage user

--- shopping/admin/manage-users.php
+++ shopping/admin/manage-users.php
@@ -22,7 +22,7 @@ if(isset($_GET['del']))
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Admin| Manage Users</title>
+	<title>Admin| Quản lí tài khoản</title>
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="css/theme.css" rel="stylesheet">
@@ -41,14 +41,14 @@ if(isset($_GET['del']))
 
 	<div class="module">
 							<div class="module-head">
-								<h3>Manage Users</h3>
+								<h3>Quản lí tài khoản</h3>
 							</div>
 							<div class="module-body table">
 	<?php if(isset($_GET['del']))
 {?>
 									<div class="alert alert-error">
 										<button type="button" class="close" data-dismiss="alert">×</button>
-									<strong>Oh snap!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
+									<strong>Lỗi!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
 									</div>
 <?php } ?>
 
@@ -59,12 +59,12 @@ if(isset($_GET['del']))
 									<thead>
 										<tr>
 											<th>#</th>
-											<th> Name</th>
+											<th> Tên</th>
 											<th>Email </th>
-											<th>Contact no</th>
-											<th>Shippping Address/City/State/Pincode </th>
-											<th>Billing Address/City/State/Pincode </th>
-											<th>Reg. Date </th>
+											<th>Liên hệ</th>
+											<th>Địa chỉ giao hàng/Thành phố/Quận/Mã PIN </th>
+											<th>Địa chỉ thanh toán/Thành phố/Quận/Mã PIN </th>
+											<th>Ngày đăng kí </th>
 										
 										</tr>
 									</thead>
